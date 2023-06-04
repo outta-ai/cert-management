eac6d135b149c07b1e3aac4ccd9c1569f8a59520
Sangwan Jeon <commits@swjeon.kr>
2023-06-04 09:03:39 +0000

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 yum groupinstall -y "Development tools"
 yum install -y gcc-c++ cairo-devel pango-devel python3-devel libjpeg-turbo-devel wget
 
-pnpm install canvas --build-from-source
+# Can I use npm here?
+npm install canvas --build-from-source
 
 if [ -n VERCEL_URL ]; then
     echo "BASE_URL=https://$VERCEL_URL" >> .env
