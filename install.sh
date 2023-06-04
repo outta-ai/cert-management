138e42821998372866a8909b9f9de413d2614366
Sangwan Jeon <commits@swjeon.kr>
2023-06-04 11:02:11 +0000

    fix: patchelf, remove build tools

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-yum groupinstall -y "Development tools"
-yum install -y gcc-c++ cairo-devel pango-devel python3-devel libjpeg-turbo-devel wget
+yum install pactchelf
 
-# Can I use npm here?
-npm install canvas --build-from-source
+wget https://zlib.net/fossils/zlib-1.2.9.tar.gz
+tar -xf zlib-1.2.9.tar.gz
+cd zlib-1.2.9
+sh configure
+make
+cp libz.so.1.2.9 ../node_modules/canvas/build/Release/libz.so.X
+cd ..
+
+patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
+patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
 
 if [ -n VERCEL_URL ]; then
     echo "BASE_URL=https://$VERCEL_URL" >> .env
